8127b29beede7c9ba26c7f1f0fbe414aee62b6b9
Samuel Penn <sam@notasnark.net>
2021-05-17 14:50:30 +0100

    Copying Swarm Docker files into git.

--- README.md
+++ README.md
@@ -9,3 +9,207 @@ pre-setup environment.
 This Docker environment is built using Helix Swarm 2021.1.
 
 This is a **TECHNICAL PREVIEW** and not tested for production use.
+
+There are two use cases that are supported, starting up Swarm against a clean
+P4D system (one which has not had Swarm configured against it), and starting
+Swarm using an existing configuration. These are described below.
+
+Note that the provided Makefile is for use on Linux, and may work on Mac. If
+you're on Windows, we recommend running the docker commands manually.
+
+---
+
+## What is Docker?
+
+Docker is a tool designed to make it easier to create, deploy, and run 
+applications by using containers. If you want to find out more about
+what these containers are, try the documentation at docker.com:
+
+https://www.docker.com/resources/what-container
+
+### Prerequisites for this Docker environment
+
+* Internet access
+* Docker-compose
+
+By default, the following port must be free to enable connection to the 
+containers from the outside world or local machine.
+
+* 80 (Swarm HTTP)
+
+These can be changed if you don't want to use these default ports. By
+default, Swarm is not configured to use HTTPS.
+
+### Containers
+
+These containers are setup to be used with Docker Compose, however it should
+be possible to run them without the use of Docker Compose. In this first 
+technical preview this hasn't been tested.
+
+#### helix.base
+
+Based on a Ubuntu 20.04 container, sets up the Perforce package repository
+and installs the Helix command line client. Used as a base by helix-swarm.
+
+#### helix.swarm
+
+Contains the Swarm and Apache server, can be configured to talk to an existing
+P4D instance.
+
+It exposes port 80 by default for access externally. If you want to use HTTPS,
+then manual configuration of the container is required.
+
+#### helix.redis
+
+Based on the standard redis container, used by helix.swarm by default as the
+local redis cache. Note that this version of redis uses the standard default
+port of 6379, whereas the one shipped with Swarm uses a default port of 7379.
+This means we need to specify the port on the command line when the server
+starts.
+
+### Data Storage
+
+Since Docker containers can be temporary, both the Swarm and Redis containers
+store their data outside of the containers on the local file system in the
+`storage` directory.
+
+## New Swarm
+
+The simplest configuration is to run up a new Swarm against an existing P4D
+server. If Swarm has been used against this P4D (Helix Server) in the past, 
+then some parts of the configuration may need to be manually done.
+
+We recommend a Unicode enabled P4D for use with Swarm. If P4D is running on
+Microsoft Windows, then you will also need to configure the triggers.
+
+First configure the .env file to be suitable for your environment. Setup the
+following variables:
+
+| Config           | Default           | Description              |
+| :---             | :---              | :---                     |
+| P4D_SUPER        | super             | Name of the super user   |
+| P4D_SUPER_PASSWD | HelixDockerBay94  | Super user password      |
+| P4D_PORT         | ssl:perforce:1666 | Port for the P4D server  |
+| SWARM_USER       | swarm             | Name of the swarm user   |
+| SWARM_PASSWD     | HelixDockerBay94  | Swarm user password      |
+| SWARM_MAILHOST   | localhost         | Mail server address      |
+| SWARM_HOST       | helix.swarm       | Hostname of Swarm server |
+| PUBLIC_HTTP      | 8666              | Port Swarm is exposed on |
+| SWARM_FORCE_EXT  | n                 | Set to 'y' to overwrite extensions |
+
+Once these have been set, then the Docker containers can be built and
+started with the following commands:
+
+```
+docker-compose build
+docker-compose up -d
+```
+
+Or alternatively, you can use the Makefile to do the same:
+
+```
+make clean build up
+```
+
+This will clean down the system, build the base Docker images, then
+bring them up. You can then use `make log` to see how the configuration
+is progressing.
+
+### Configuration steps
+
+If there is an existing `config.php` file in the data directory, then
+configuration will be skipped, and we assume that Swarm is already
+configured.
+
+The following steps are followed when Swarm is configured:
+
+* Check for a running P4D server against {P4D_PORT}.
+* Check that we can log into it with {P4D_SUPER} and {P4D_SUPER_PASSWD}.
+* Check to see that there is a {SWARM_USER}. If there is, we don't create
+  one, otherwise we will create one with the given {SWARM_PASSWD}
+
+
+Once the Swarm container is up and running, and Swarm is working, the
+only entry in the .env file that is still needed is `PUBLIC_HTTP`. At the
+very least, you may want to remove the passwords from this file.
+
+If the P4D server is a Linux server, and no Swarm extensions are configured
+on it, then Swarm extensions will be automatically installed.
+
+If the SWARM_FORCE_EXT environment variable is set to "y", then any existing
+Swarm extensions will be removed, and new ones always installed.
+
+
+### Managing the Container
+
+You can stop and start the container with:
+
+```
+docker-compose stop
+docker-compose up -d
+```
+
+Alternatively, using the included Makefile:
+
+```
+make stop
+make up
+```
+
+You can also gain access to a running Swarm container with `make bash`
+
+Since the container will not perform any reconfiguration if the config.php
+file already exists, if a new container is started after the previous has
+been stopped or even deleted then it will simply pick up the previous
+configuration and continue from where it left off.
+
+The exception is that if there is a perforce-swarm-site.conf in the external
+data/etc directory, then this will be copied into place for use by the
+running Apache server if it isn't there already.
+
+  
+
+## Migrate Swarm
+
+If you already have a Swarm installation, then you may want to just move
+it into Docker. We don't recommend this for production servers at the moment
+(by this, we mean that we haven't yet performed extensive testing of Swarm
+in Docker, and we don't have built in support for HTTPS).
+
+* Copy the contents of the `data` directory into the `swarm-data` directory.
+* Start the docker containers
+
+Swarm will automatically pick up the `config.php` and use it 'as is'. No 
+changes will be made to it. This may mean that you need to configure the Redis
+configuration if you want to use the Dockerised Redis server. In which case
+point the Swarm redis configuration at helix.redis:
+
+```
+    'redis' => [
+        'options' => [
+            'server' => [
+                'host' => 'helix.redis',
+            ],
+        ],
+    ],
+```
+
+You may also need to change the host name in the env file.
+
+If you have triggers already installed on your P4D, then you will need to 
+make sure that they are configured to point at the new Swarm location.
+
+As above, if there is a perforce-swarm-site.conf file in the data/etc 
+directory, then this will be copied and used by Apache. Otherwise a new 
+Apache config will be created using the SWARM_HOST from the env file.
+
+
+---
+
+ @copyright   2021 Perforce Software. All rights reserved.
+
+---
+ @license     Please see LICENSE in top-level folder of this distribution.
+
+
+
---
+++ containers/helix-swarm/setup/swarm-docker-setup.sh
@@ -0,0 +1,208 @@
+#!/bin/bash
+
+SWARM_HOME="/opt/perforce/swarm"
+SETUPDIR="/opt/perforce/setup"
+
+LOG="${SWARM_HOME}/data/docker.log"
+
+function log {
+    echo $(date +"%Y/%m/%d %H:%M:%S") - $@ 
+}
+
+log "--"
+log "Starting swarm-docker-setup.sh"
+
+
+function die {
+    log $@
+    exit 1
+}
+
+
+#
+# Wait for P4D to startup.
+#
+function waitForP4D {
+    log "Checking P4D '${P4D_PORT}' to make sure it is running."
+
+    local ATTEMPTS=0;
+    while [ ${ATTEMPTS} -lt ${P4D_GRACE:-30} ]
+    do
+        if [[ ${P4D_PORT} =~ ssl:.* ]]
+        then
+            p4 -p${P4D_PORT} trust -fy || log "Failed to trust SSL on '${P4D_PORT}'"
+        fi
+        if p4 -p ${P4D_PORT:-1666} -ztag info -s
+        then
+            log "Contact!"
+            return 0
+        fi
+        log "Waiting after ${ATTEMPTS}"
+        ATTEMPTS=$((ATTEMPTS + 1))
+        sleep 1
+    done
+    
+    # Failed
+    return 1
+}
+
+function configureP4D {
+    # Install Extensions / Triggers
+    
+    # Check to see if Swarm triggers or extensions are already installed
+    NEEDCONFIG=0
+    $P4D triggers -o | grep -q "swarm"
+    NEEDCONFIG=$?
+    if [ $NEEDCONFIG -ne 0 ]
+    then
+        $P4D extension --list --type extensions | grep -q "swarm"
+        NEEDCONFIG=$?
+        if [ $NEEDCONFIG -eq 0 ]
+        then
+            log "Detected Swarm extensions already installed."
+            
+            if [ "${SWARM_FORCE_EXT}" = "y" ]
+            then
+                $P4D extension --delete Perforce::helix-swarm -y
+                log "Deleting existing Swarm extension to force re-configuration."
+                NEEDCONFIG=1
+            else
+                log "You will need to re-configure them with token ${SWARM_TOKEN} to point at this Swarm instance."
+            fi
+            
+        fi
+    else
+        log "Detected Swarm triggers already installed."
+        log "You will need to re-configure them with token ${SWARM_TOKEN} to point at this Swarm instance"
+    fi
+    
+    if [ $NEEDCONFIG -ne 0 ]
+    then
+        $P4D info | grep -q P4D/LINUX
+        if [ $? -eq 0 ]
+        then
+            log "Configure ${P4D_PORT} to use Swarm extensions"
+            echo ${P4D_SUPER_PASSWD} | $P4D login
+            $P4D extension --yes --allow-unsigned --install ${SWARM_HOME}/p4-bin/helix-swarm.p4-extension
+            
+            $P4D extension --configure Perforce::helix-swarm -o > /tmp/global.txt
+            sed -i "s#... SWARM-TOKEN#${SWARM_TOKEN}#g" /tmp/global.txt
+            sed -i "s#/localhost/#/${SWARM_HOST}/#g" /tmp/global.txt
+            sed -i "s#sampleExtensionsUser#${P4D_SUPER}#g" /tmp/global.txt
+            $P4D extension --configure Perforce::helix-swarm -i < /tmp/global.txt
+            rm -f /tmp/global.txt
+            
+            $P4D extension --configure Perforce::helix-swarm --name swarm -o | $P4D extension --configure Perforce::helix-swarm --name swarm -i
+            log "Configured Swarm extensions against ${P4D_PORT}"
+        else
+            log "The server at ${P4D_PORT} is not a Linux server, so cannot configure Swarm extensions"
+            log "You will need to configure Triggers on this server yourself using the token ${SWARM_TOKEN}"
+        fi
+    else
+        log "Swarm extensions have not been configured."
+    fi
+}
+
+function configureSwarm {
+    log "Swarm does not appear to be configured, configuring it against '${P4D_PORT}'."
+    
+    # Give p4d a bit of time to startup
+    waitForP4D || die "Unable to contact P4D server at '${P4D_PORT}'"
+    
+    log "Connected to P4D, beginning configuration check."
+    
+    P4D="p4 -p${P4D_PORT} -u${P4D_SUPER}"
+        
+    $P4D -ztag info | grep -q "unicode enabled" || log "*** The P4D server at '${P4D_PORT}' is not unicode enabled. We STRONGLY recommend using a Unicode server with Swarm ***"
+    
+    
+    # Login to the server as the super user
+    echo ${P4D_SUPER_PASSWD} | $P4D login || die "Unable to login to '${P4D_PORT}' as user '${P4D_SUPER}' with '${P4D_SUPER_PASSWD}'"
+
+    log "Logged in"
+
+    # Does the Swarm user already exist?
+    CREATE=""
+    $P4D users | grep "${SWARM_USER} <" >> $LOG
+    $P4D users | grep -q "${SWARM_USER} <" || CREATE="-c"
+
+    # Base Swarm configuration
+    /opt/perforce/swarm/sbin/configure-swarm.sh -n \
+        -p "${P4D_PORT}" -U "${P4D_SUPER}" -W "${P4D_SUPER_PASSWD}" \
+        -u "${SWARM_USER}" -w "${SWARM_PASSWD}" $CREATE -g \
+        -H "${SWARM_HOST}" -e "${SWARM_MAILHOST}" >> $LOG
+    
+    if [ $? -ne 0 ]
+    then
+        log "configure-swarm.sh failed, using the following parameters:"
+        log "-p \"${P4D_PORT}\" -U \"${P4D_SUPER}\" -W \"${P4D_SUPER_PASSWD}\""
+        log "-u \"${SWARM_USER}\" -w \"${SWARM_PASSWD}\" $CREATE -g"
+        log "-H \"${SWARM_HOST}\" -e \"${SWARM_MAILHOST}\""
+        exit 1
+    fi
+    
+    log "Successfully configured Swarm"
+    
+    # Get a ticket that stays valid if the container restarts
+    SWARM_TICKET=$(echo ${SWARM_PASSWD} | p4 -p ${P4D_PORT} -u ${SWARM_USER} login -ap | grep -v word)
+    sed -i "s/\('password' => '\)[0-9A-F]*/\1${SWARM_TICKET}/" ${SWARM_HOME}/data/config.php
+
+    # Create a new Swarm token
+    mkdir -p ${SWARM_HOME}/data/queue/tokens
+    mkdir -p ${SWARM_HOME}/data/queue/workers
+    SWARM_TOKEN=$(uuid)
+    touch ${SWARM_HOME}/data/queue/tokens/${SWARM_TOKEN}
+    
+    log "Generated a swarm token of ${SWARM_TOKEN}"
+    
+    sed -i "s/);/    'log' => [\n        'priority' => 7,\n    ],\n    'redis' => [\n        'options' => [\n            'server' => [\n                'host' => '$SWARM_REDIS',\n            ],\n        ],\n    ],\n);/" ${SWARM_HOME}/data/config.php
+    
+    rm -f ${SWARM_HOME}/data/cache/*
+    rm -f ${SWARM_HOME}/data/p4trust
+    chown -R www-data:www-data ${SWARM_HOME}/data
+}
+
+function configureApacheOnly {
+    log "Need to configure Apache"
+    if [ -f ${SWARM_HOME}/data/etc/perforce-swarm-site.conf ]
+    then
+        cp ${SWARM_HOME}/data/etc/perforce-swarm-site.conf /etc/apache2/sites-available/perforce-swarm-site.conf
+    else
+        sed -e 's#APACHE_LOG_DIR#/var/log/apache2#' -e "s#REPLACE_WITH_SERVER_NAME#${SWARM_HOST}#" /opt/perforce/etc/perforce-swarm-site.conf > /etc/apache2/sites-available/perforce-swarm-site.conf
+    fi
+    a2ensite perforce-swarm-site
+    a2enmod  rewrite
+}
+
+# Only configure swarm if the perforce-swarm-site is not already defined
+if  [ -f ${SWARM_HOME}/data/config.php ]
+then
+    if apachectl -S | grep -q "swarm"
+    then
+        log "Everything seems to be configured."        
+    else
+        configureApacheOnly
+    fi
+    if [ -f ${SWARM_HOME}/data/etc/swarm-cron-hosts.conf ]
+    then
+        cp ${SWARM_HOME}/data/etc/swarm-cron-hosts.conf /opt/perforce/etc/swarm-cron-hosts.conf
+    fi
+else
+    configureSwarm
+    configureP4D
+fi
+
+# Make sure that we have a copy of the configuration files.
+mkdir -p ${SWARM_HOME}/data/etc
+#cp /opt/perforce/etc/swarm-cron-hosts.conf ${SWARM_HOME)/data/etc
+#cp /etc/apache2/sites-available/perforce-swarm-site.conf ${SWARM_HOME)/data/etc
+
+log "Swarm setup finished."
+
+
+# We need Cron running, but we want Apache to run as a foreground process so that Docker can track it.
+# Since the configuration script starts Apache, we need to ensure it is stopped.
+service cron start
+apache2ctl stop
+
+exec apache2ctl -D FOREGROUND
